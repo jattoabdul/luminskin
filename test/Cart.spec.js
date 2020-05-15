d3be7ea6a0f0dc4469a28d76dddf580b5167d41f
jattoabdul <jattoade@gmail.com>
2020-05-15 14:04:49 +0100

    commence testing of cart component

--- test/Cart.spec.js
+++ test/Cart.spec.js
@@ -1,19 +1,20 @@
 import { mount, shallowMount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Cart from '@/components/Cart.vue'
 
+// @TODO: Fix Tests
 const factory = () => {
-  return shallowMount(Logo, {
+  return shallowMount(Cart, {
   })
 }
 
-describe('Logo', () => {
+describe('Cart', () => {
   test('is a Vue instance', () => {
     const wrapper = factory()
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 
   test('renders correctly', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(Cart)
     expect(wrapper.element).toMatchSnapshot()
   })
 })
