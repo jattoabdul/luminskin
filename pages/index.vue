0a7491c256f0d50bc9c607913e36ff9ecc82d4ad
jattoabdul <jattoade@gmail.com>
2020-05-15 06:35:31 +0100

    outline application structure and methods in root component

--- pages/index.vue
+++ pages/index.vue
@@ -1,84 +1,58 @@
 <template>
   <div class="container dark-color">
+    <!-- nav -->
+    <nav class="nav-bar">
+      <span>
+        Lumin
+      </span>
+      <ul>
+        <li>
+          Shop
+        </li>
+        <li>
+          Learn
+        </li>
+      </ul>
+      <span>
+        Account
+      </span>
+      <!-- cart icon & count -->
+      <span @click.stop="showCart = !showCart" class="cart-count">
+        cartsize: {{ cart | cartSize }}
+      </span>
+      <!--end of cart icon & count -->
+    </nav>
+    <!-- end of nav -->
+
     <!-- header -->
-    <h1>
-      Luminskin All Products
-    </h1>
+    <div class="header">
+      <div>
+        <h1>
+          All Products
+        </h1>
+      </div>
+      <div>
+        <span>
+          Filter
+        </span>
+      </div>
+    </div>
     <!-- end of header -->
 
-    <br>
-    <br>
-
     <!-- product list -->
-    <ul>
-      <!-- single product -->
-      <li v-for="product in products" :key="product.id">
-        <img :src="product.image_url" :alt="product.title">
-        <p>
-          {{ product.title }} - {{ product.price | currency({symbol: getCurrencySymbol(currentCurrency.toLowerCase())}) }}
-        </p>
-        <button @click.stop="addToCart(product)">
-          Add to Cart
-        </button>
-      </li>
-      <!-- end of single product -->
-    </ul>
+    <product-list :products="products" :current-currency="currentCurrency" />
     <!-- end of product list -->
-
-    <br>
-    <br>
-
-    <!-- cart icon -->
-    <p @click.stop="showCart = !showCart" class="cart-count">
-      cartsize: {{ cart | cartSize }}
-    </p>
-    <!--end of cart icon -->
-
-    <br>
 
     <!-- cart content -->
     <div v-show="showCart" class="cart">
-      <!-- currency selector -->
-      <p @click.stop="showCurrencySelector = !showCurrencySelector">
-        Select Currency
-      </p>
-      <ul v-show="showCurrencySelector" class="currency-selector">
-        <li v-for="(eachCurrency, index) in currency" :key="index" @click.stop="selectCurrency(index)">
-          {{ eachCurrency }} - currency
-        </li>
-      </ul>
-      <!-- end of currency selector -->
-      <br>
-      <br>
-      <ul>
-        <!-- single cart item -->
-        <li v-for="cartItem in cart" :key="cartItem.id">
-          {{ cartItem.title }}
-          <button @click.stop="removeProduct(cartItem)">
-            x
-          </button>
-          <button @click.stop="quantityChange(cartItem, 'incriment')">
-            +
-          </button>
-          {{ cartItem.quantity }}
-          <button @click.stop="quantityChange(cartItem, 'decriment')">
-            -
-          </button>
-          {{ cartItem.price | currency({symbol: getCurrencySymbol(currentCurrency.toLowerCase())}) }}
-          <img :src="cartItem.image_url" :alt="cartItem.title">
-        </li>
-        <!-- end of single cart item -->
-      </ul>
-      <p>
-        cartSubtotal: {{ cartSubTotal | currency({symbol: getCurrencySymbol(currentCurrency.toLowerCase())}) }}
-      </p>
-      <button @click.stop="clearCart()">
-        Clear Cart
-      </button>
-      <br>
-      <button @click.stop="proceedToCheckout()" :disabled="cart.length <= 0">
-        Proceed To checkout
-      </button>
+      <cart
+        :products="products"
+        :show-currency-selector="showCurrencySelector"
+        :currency="currency"
+        :cart="cart"
+        :current-currency="currentCurrency"
+        :tax="tax"
+      />
     </div>
     <!-- end cart content -->
   </div>
@@ -86,13 +60,13 @@
 
 <script>
 import gql from 'graphql-tag'
-// import ProductList from '~/components/ProductList'
-// import Cart from '~/components/Cart'
+import ProductList from '~/components/ProductList'
+import Cart from '~/components/Cart'
 
 export default {
   components: {
-    // ProductList,
-    // Cart
+    ProductList,
+    Cart
   },
   filters: {
     cartSize (cart) {
@@ -111,7 +85,6 @@ export default {
       cart: [],
       tax: 0.065,
       cartSubTotal: 0,
-      cartTotal: 0,
       checkoutBool: false,
       showCart: false,
       showCurrencySelector: false
@@ -142,6 +115,29 @@ export default {
       }
     `
   },
+  mounted() {
+    this.$eventBus.$on('add-to-cart', payload => {
+      this.addToCart(payload.product)
+    })
+    this.$eventBus.$on('select-currency', payload => {
+      this.selectCurrency(payload.index)
+    })
+    this.$eventBus.$on('remove-product', payload => {
+      this.removeProduct(payload.product)
+    })
+    this.$eventBus.$on('quantity-change', payload => {
+      this.quantityChange(payload.product, payload.action)
+    })
+    this.$eventBus.$on('clear-cart', _ => {
+      this.clearCart()
+    })
+    this.$eventBus.$on('proceed-to-checkout', _ => {
+      this.proceedToCheckout()
+    })
+    this.$eventBus.$on('toggle-currency-selector', _ => {
+      this.toggleCurrencySelector()
+    })
+  },
   methods: {
     addToCart (product) {
       let found = false
@@ -160,16 +156,11 @@ export default {
         product.quantity = 1
         this.cart.push(product)
       }
-
-      this.cartSubTotal = this.cartSubTotal + product.price
-      this.cartTotal = this.cartSubTotal + (this.tax * this.cartSubTotal)
       this.checkoutBool = true
     },
     removeProduct (product) {
       const index = this.cart.indexOf(product)
       this.cart.splice(index, 1)
-      this.cartSubTotal = this.cartSubTotal - (product.price * product.quantity)
-      this.cartTotal = this.cartSubTotal + (this.tax * this.cartSubTotal)
 
       if (this.cart.length <= 0) {
         this.checkoutBool = false
@@ -195,21 +186,12 @@ export default {
             if (newProduct.quantity === 0) {
               const index = this.cart.indexOf(newProduct)
               this.cart.splice(index, 1)
-              this.cart.splice(i, 1)
             } else {
               this.$set(this.cart, i, newProduct)
             }
           }
         }
       }
-
-      if (direction === 'incriment') {
-        this.cartSubTotal = this.cartSubTotal + product.price
-      } else {
-        this.cartSubTotal = this.cartSubTotal - product.price
-      }
-
-      this.cartTotal = this.cartSubTotal + (this.tax * this.cartSubTotal)
 
       if (this.cart.length <= 0) {
         this.checkoutBool = false
@@ -229,17 +211,18 @@ export default {
       console.log('checking out this cart =>', this.cart)
       //  toast cart checked out
       this.clearCart()
+    },
+    toggleCurrencySelector() {
+      this.showCurrencySelector = !this.showCurrencySelector
     }
   }
 }
 </script>
 
-<style>
-.container {
-  /* @apply min-h-screen flex justify-center items-center text-center mx-auto; */
-  display: flex;
-  flex-direction: row;
-}
+<style scoped lang="scss">
+/* .container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+} */
 .ash-color {
   color: #e2e6e3;
 }
