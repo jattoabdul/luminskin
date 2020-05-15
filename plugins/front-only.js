14353cf661740b2622ea95d21fa63d8fb245ff9b
jattoabdul <jattoade@gmail.com>
2020-05-15 06:54:49 +0100

    fix currency filter error by moving it to a universal plugin wiht ssr enabled

--- plugins/front-only.js
+++ plugins/front-only.js
@@ -2,14 +2,6 @@ import Vue from 'vue'
 
 import Ripple from 'vue-ripple-directive'
 import VueScrollTo from 'vue-scrollto'
-import VueCurrencyFilter from 'vue-currency-filter'
 
 Vue.directive('ripple', Ripple)
 Vue.use(VueScrollTo)
-Vue.use(VueCurrencyFilter, {
-  thousandsSeparator: ',',
-  fractionCount: 2,
-  fractionSeparator: '.',
-  symbolPosition: 'front',
-  symbolSpacing: false
-})
--- plugins/universal.js
+++ plugins/universal.js
@@ -1,3 +1,11 @@
 import Vue from 'vue'
+import VueCurrencyFilter from 'vue-currency-filter'
 
 Vue.prototype.$eventBus = new Vue()
+Vue.use(VueCurrencyFilter, {
+  thousandsSeparator: ',',
+  fractionCount: 2,
+  fractionSeparator: '.',
+  symbolPosition: 'front',
+  symbolSpacing: false
+})
